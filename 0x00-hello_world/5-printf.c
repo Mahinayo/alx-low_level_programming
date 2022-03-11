49090ce41d145ce7d608f3460358842f2cc29d01
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-10 23:17:48 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the output
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

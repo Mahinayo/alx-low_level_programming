3cb0279d0c89484aea707f0b0f7ff9b0ae8a4d30
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-12 01:29:12 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@ int main(void)
 {
 char lowwer_case;
 char upper_case;
-for (upper_case = A ; upper_case <= Z ; upper_case++)
+for (upper_case = ' A ' ; upper_case <= ' Z ' ; upper_case++)
 {
 putchar(upper_case'\n');
 }
-for (lower_case = a ; lower_case <= z ; lower_case++)
+for (lower_case = ' a ' ; lower_case <= ' z ' ; lower_case++)
 {
 putchar(lower_case'\n');
 return (0);

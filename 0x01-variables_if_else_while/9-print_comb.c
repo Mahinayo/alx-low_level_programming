9331c3a2d9d19b969919685b0e21e0184b0c8d25
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-14 01:54:49 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print combination
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int c;
+for (c = '0' ; c <= '9' ; c++)
+{
+putchar (c);
+putchar (',');
+putchar (' ');
+}
+putchar ('\n');
+return (0);
+}

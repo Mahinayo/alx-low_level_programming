833b89d0696310cf5a89c3b2433ff455cd7af342
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-14 01:02:20 -0700

    without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabets without e and q
+ *
+ * Retunr: always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+if (c == 'e')
+continue;
+else if (c == 'q')
+continue;
+putchar (c);
+}
+putchar ('\n');
+return (0);
+}

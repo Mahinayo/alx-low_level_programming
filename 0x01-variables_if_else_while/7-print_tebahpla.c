059655c521c64d6c7ef971a1d32ac469f3c5678c
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-14 01:30:01 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - revers alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char a;
+for (a = 'z' ; a >= 'a' ; a--)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}

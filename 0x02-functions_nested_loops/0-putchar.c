ba3b761c1adfd8b1a4097fceb72ac6bcc17d218e
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-16 00:22:38 -0700

    main

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - correct output
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - print alphabet 10x
+ *
+ * Return: the correct output
+ */
+void print_alphabet_x10(void)
+{
+int a;
+char b;
+for (a = 0 ; a < 10 ; a++)
+{
+for (b = 'a' ; b <= 'z' ; b++)
+{
+putchar (b);
+}
+putchar ('\n');
+}
+}

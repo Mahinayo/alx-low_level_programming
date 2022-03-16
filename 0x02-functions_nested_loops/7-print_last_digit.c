67cb577ce1d5459d65c52abba52364d428aea58f
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-16 02:38:11 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: take integer
+ * Return: the modular
+ */
+int print_last_digit(int n)
+{
+
+if (n < 0)
+{
+return (-1 (n % 10));
+}
+else
+{
+return (n % 10);
+}
+}

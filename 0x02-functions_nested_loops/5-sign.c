ef0298a10654ba7bacce4c3f37100c9800a5678d
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-16 02:07:14 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 _putchar ('+');
-_putchar ('n');
+return (1);
 }
 else
 if (n < 0)
 {
-return (-1);
 _putchar ('-');
-_putchar ('n');
+return (-1)
 }
 else
 {
+_putchar ('0');
 return (0);
-_putchar (0);
-_putchar ('n');
 }
 }

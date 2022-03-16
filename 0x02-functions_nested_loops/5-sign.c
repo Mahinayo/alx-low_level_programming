9c747a2af9fd0a17361767371063d451bfe966d2
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-16 01:54:18 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,19 @@ if (n > 0)
 {
 return (1);
 _putchar ('+');
+_putchar ('n');
 }
 else
 if (n < 0)
 {
 return (-1);
 _putchar ('-');
+_putchar ('n');
 }
 else
 {
 return (0);
 _putchar (0);
+_putchar ('n')
 }
 }

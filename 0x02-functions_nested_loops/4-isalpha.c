74dd0dea7fe13b5995eea52d3cf13c981ad3adc2
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-16 01:23:32 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - return 1 if is it alpha
+ * @c: take char
+ * Return: 0 if it is not alpha
+ */
+int _isalpha(int c
+{
+if (c >= 'A' && c <= 'Z') || (c >= 'a' && c<= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
+		

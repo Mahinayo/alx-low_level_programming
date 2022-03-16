165cff89729af231b12da7c6c2f76de8cea26b60
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-16 01:44:49 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.c"
+/**
+ *print_sign - return 1 and print +
+ *@n: take char
+ *Return: 0 or -1 
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar ('+');
+}
+else
+if (n < 0)
+{
+return (-1);
+_putchar ('-');
+}
+else
+{
+return (0);
+_putchar (0);
+}

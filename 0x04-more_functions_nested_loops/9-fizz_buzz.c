f9ec7d177a81060bbc8daffda6b36765c411ae48
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-18 03:36:23 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - fizz buzz
+ *
+ * Return:0
+ */
+int main(void)
+{
+int x;
+int i;
+x = 100;
+i = 1;
+while (i <= x)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i ", i);
+}
+i++;
+}
+printf("\n");
+return (0);
+}

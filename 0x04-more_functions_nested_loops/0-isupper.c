522e1f00a2e1b6dd50567de579276e8ea22fc7f9
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-17 23:35:48 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - return 1 if it is upper case
+ * @c: take char
+ * Return: 0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}

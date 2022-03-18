9109868101a319794f9be0e7f572ee55aaac84c9
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-18 01:42:43 -0700

    most numbersclaerc

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,8 @@ void print_most_numbers(void)
 int i;
 for (i = 0 ; i <= 9 ; i++)
 {
-if (i == 2 || i == 4)
-{
-continue;
-}
-else
-{
-putchar ('i');
+if (i != 2 || i != 4)
+putchar (i + '0');
 putchar ('\n');
 }
 }

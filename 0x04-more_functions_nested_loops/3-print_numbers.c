f85ab4a65440712ccade5a29ccb7f20a3e5f8c7d
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-18 01:21:04 -0700

    num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - print numbers
+ *
  * Return: 0
  */
 void print_numbers(void)
 {
 int i;
-i = 0;
-while (i < 10)
-{
+for (i = 0 ; i <= 9 ; i++)
 _putchar (i + '0');
-i++;
-}
 _putchar ('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
+void _putchar(void);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);

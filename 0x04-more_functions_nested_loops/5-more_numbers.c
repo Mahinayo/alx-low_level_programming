52e77f8620ff797f92b5130d837621cf27c8810e
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-18 02:00:30 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+int i;
+int n;
+for (n = 1 ; n <= 10 ; n++)
+{
+for (i = 0 ; i <= 14 ; i++)
+{
+putchar (i / 10 + '0');
+putchar (i % 10 + '0');
+}
+putchar ('\n');
+}
+}

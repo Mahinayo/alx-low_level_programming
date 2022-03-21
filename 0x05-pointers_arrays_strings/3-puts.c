999e0bcf465c1aaf4c6f3608b271d9c1ce17d210
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-21 04:05:22 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print string
+ * @str: string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}

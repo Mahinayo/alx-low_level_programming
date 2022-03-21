ab3d599eaed729d9253b0d0aca108fda07eef583
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-21 04:50:48 -0700

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,13 @@ int j;
 char a[500];
 i = 0;
 j = 0;
-while (*(s + i));
+while (*(s + i))
 {
 *(a + i) = *(s + i);
 i++;
 }
 i = i - 1;
-while (i >= 0);
+while (i >= 0)
 {
 *(s + i) = *(a + j);
 j++;

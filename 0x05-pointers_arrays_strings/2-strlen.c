3d80dc330440e1f8bdce82ead67d1a5d02313931
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-21 04:00:08 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s: string pointer
+ * Return: s
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0 ; s[i] != '\0' ; i++)
+{
+continue;
+}
+return (i);
+}

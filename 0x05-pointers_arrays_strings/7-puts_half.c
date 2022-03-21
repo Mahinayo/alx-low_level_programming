21994e064e6a36867b03b55cbd008593c20e67a6
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-21 04:56:53 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - print half
+ * @str: string
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+int i;
+int j;
+i = 0;
+while (str[i])
+i++;
+j = i / 2;
+if (i % 2)
+j += i;
+while (j < i)
+{
+putchar(str[j]);
+}
+putchar('\n');
+}

2a3ac3789a52e7d1eada82a5cece79252fb7b811
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-21 04:41:19 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - every other char
+ * @str: string
+ * Return:0
+ */
+void puts2(char *str)
+{
+int i;
+int a;
+i = 0;
+a = 0;
+while(str[i++]);
+{
+a++;
+}
+for (i = 0 ; i <= a ; i += 2)
+{
+putchar(*(str + i));
+}
+putchar('\n');
+}

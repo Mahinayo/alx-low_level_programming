d039a05ca1d5102177183e07e9015554a5bf9917
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-24 00:19:52 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - encode a string using rot13
+ * @r: char r
+ * Return: r
+ */
+char *rot13(char *r)
+{
+int i;
+i = 0;
+while (r[i] != '\0')
+{
+while ((r[i] >= 'a' && r[i] <= 'z') || (r[i] >= 'A' && r[i] <= 'Z'))
+{
+if ((r[i] >= 'a' && r[i] <= 'm') || (r[i] >= 'A' && r[i] <= 'M'))
+r[i] += 13;
+else
+r[i] += 13;
+i++;
+}
+i++;
+}
+return (r);
+}

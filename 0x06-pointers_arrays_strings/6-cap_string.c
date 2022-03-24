4b3d342b26b94f40de020edced740d473e7c5e28
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-23 23:49:50 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalized all words
+ * @s: string s
+ * Return: 0
+ */
+char *cap_string(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t' || s[i - 1] == ',' || s[i - 1] == '.' || s[i - 1] == ';' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}') && (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] = s[i] - 32;
+}
+else if ((s[0] >= 97 && s[0] <= 122))
+{
+s[0] = s[0] - 32'
+}
+else
+{
+s[i] = s[i];
+}
+i++;
+}
+return (s);
+}

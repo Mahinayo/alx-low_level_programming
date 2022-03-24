33185a8470eb24d9059f3bc169dbfdebb7a617e1
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-23 23:17:41 -0700

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - lowercase to uppercase
+ * @n: char n
+ * Return: 0
+ */
+char *string_toupper(char *n)
+{
+int i;
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+n[i] = n[i] - 32;
+i++;
+}
+return (n);
+}

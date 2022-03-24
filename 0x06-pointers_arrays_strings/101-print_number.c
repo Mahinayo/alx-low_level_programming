46bba8a9cc8ef3b2c86d9822ad487433821c43e9
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-03-24 01:11:00 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * base10 - base 10
+ * @n: int
+ * Return: 0
+ */
+int base10(int n)
+{
+int base = 10;
+while (n > 0)
+{
+base *= 10;
+n--;
+}
+return (base);
+}
+/**
+ * print_number - print an integer
+ * @n: int
+ * Return: 0
+ */
+void print_number(int n)
+{
+int power;
+power = base10(8);
+if (n < 0)
+{
+putchar('-');
+n *= -1;
+}
+if (n == 0)
+putchar('0');
+else
+{
+while (n / power == 0)
+power /= 10;
+while (power >= 1)
+{
+putchar((n / power) +'0');
+n %= power;
+power /= 10;
+}
+}
+}

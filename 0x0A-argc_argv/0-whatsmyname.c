1ace88b6cf09a605dcaf2e733368240d8d95210e
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-04-04 03:35:16 -0700

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+int main(int agrc, char* argv[])
+++){p
+
+printf("%s", argv[i])
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main
+ *@argc: argc
+ * Return: 0
+ */
+int main(int argc)
+{
+printf("%d\n", argc);
+return (0);
+}

df86bb83740f4b62e6c72497b9dce964ae257834
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-04-05 03:35:28 -0700

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,11 @@
  * @argv: argv
  * Return:0
  */
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
+int i;
 (void)argv;
-printf("%d\n", argc);
+for (i = 0; i < argc ; i++)
+printf("%d\n", i);
 return (0);
 }

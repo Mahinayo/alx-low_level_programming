5d734baeb3eca9f2b77ce4f3123e61ea341c9997
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-04-08 00:35:13 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - allocate memory
+ * @b: unsigned int b
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+char *c;
+int *i;
+float *f;
+double *d;
+c = (char*) malloc(b * sizeof(char));
+{
+return (c);
+}
+i = (int*) malloc(b * sizeof(int));
+{
+return (i);
+}
+f = (float*) malloc(f * sizeof(float));
+{
+return (f);
+}
+}

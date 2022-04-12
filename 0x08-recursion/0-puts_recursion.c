42fd972c9840f5778856531da3e738724a309197
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-12 12:02:11 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,17 @@
-uu
+#include "main.h"
+
+/**
+ * _puts_recursion - main - check the code
+ * @s: string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*(s) == '\0')
+		_putchar('\n');
+}

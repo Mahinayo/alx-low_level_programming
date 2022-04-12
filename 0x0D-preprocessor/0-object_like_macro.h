aa31a5a2ac1722680150c45752091915e7c56eb9
Mahinayo <mahaletyohannesgh11@gmail.com>
2022-04-12 01:24:17 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#include "SIZE.h"
+#include "main.h"
+/**
+ * main - main
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+foo = (char *) malloc(size);
+return (0);
+}
+
---
+++ 0x0D-preprocessor/SIZE.h
@@ -0,0 +1,7 @@
+#ifndef SIZE_H
+#define SIZE 1024
+struct foo
+{
+int m;
+};
+#endif

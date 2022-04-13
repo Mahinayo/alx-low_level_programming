3d78235519922c3ff1e17fe1c2ca39a69c893526
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-13 14:03:01 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,29 @@
-oo
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer
+ * @array: array
+ * @size: number of element
+ * @cmp: pointer
+ * Return: nothing
+**/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array == NULL || cmp == NULL)
+{
+return (-1);
+}
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+return (-1);
+}

f67f6d260f19336f8c2750de37b68154449f6e60
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-13 13:22:10 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,15 @@
 #ifndef FUNCTION_POINTERS_H 
 #define FUNCTION_POINTERS_H
+/**
+* struct function_pointers - function pointers
+*@name: name of the person
+*Description: structur with name
+**/
+typedef struct function_pointers
+{
+  char *name;
+}function_pointers_t;
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));

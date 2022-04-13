92249bdfaf3739af6408eb162e6df1d711b36e77
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-13 13:33:35 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,19 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
-* print_name - print name
-*@name: name
-*@f: pointer
-*Return: 0
-*/
+ * print_name - print name
+ * @name: name
+ * @f: pointer
+ * Return: 0
+**/
 void print_name(char *name, void (*f)(char *))
 {
- if (name == NULL || f == NULL)
- {
-   return;
- }
-  else
-  {
-    f(name);
-  }
+if (name == NULL || f == NULL)
+{
+return;
+}
+else
+{
+f(name);
+}
 }

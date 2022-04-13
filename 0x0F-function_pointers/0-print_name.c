866cd2066f443a2f97d5b6b2a806cab5addb08a8
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-13 12:35:46 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,16 @@
-uy
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - print name
+*@name: name
+*Return: 0
+*/
+void print_name(char *name, void (*f)(char *))
+{
+  printf("%s\n", name);
+}
+int main()
+{
+  void(*f)(char *);
+  f = print_name;
+}

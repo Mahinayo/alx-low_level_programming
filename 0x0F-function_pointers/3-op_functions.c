df1f5fcfa04063fde49c7e2dbd3d2e52008aeab8
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-13 14:24:26 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,1 +1,50 @@
-kk
+/**
+ * op_add - add two numbers
+ * @a: int a
+ * @b: int b
+ * Return: result
+**/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - substracts two numbers
+ * @a: int a
+ * @b: int b
+ * Return: result
+**/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiply two numbers
+ * @a: int a
+ * @b: int b
+ * Return: result
+**/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: int a
+ * @b: int b
+ * Return: result
+**/
+int op_sub(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - modules two numbers
+ * @a: int a
+ * @b: int b
+ * Return: result
+**/
+int op_mod(int a, int b)
+{
+return (a % b);
+}

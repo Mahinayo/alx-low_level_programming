84d6f3e3865f46a60a8a33cd8ad6be008068b368
Mahinayo <99645691+Mahinayo@users.noreply.github.com>
2022-04-13 14:28:41 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,49 +1,56 @@
 /**
- * op_add - add two numbers
- * @a: int a
- * @b: int b
- * Return: result
+ * op_add - Adds two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
 **/
 int op_add(int a, int b)
 {
 return (a + b);
 }
+
 /**
- * op_sub - substracts two numbers
- * @a: int a
- * @b: int b
- * Return: result
+ * op_sub - subtracts two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
 **/
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
+
 /**
- * op_mul - multiply two numbers
- * @a: int a
- * @b: int b
- * Return: result
+ * op_mul - Multiplies two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
 **/
+
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+
 /**
- * op_div - divide two numbers
- * @a: int a
- * @b: int b
- * Return: result
+ * op_div - Divides two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
 **/
-int op_sub(int a, int b)
+
+int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
+
 /**
- * op_mod - modules two numbers
- * @a: int a
- * @b: int b
- * Return: result
+ * op_mod - Modules two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
 **/
+
 int op_mod(int a, int b)
 {
 return (a % b);
